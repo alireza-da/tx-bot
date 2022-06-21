7cce9eee8890812faaf0426412e2b390ccd35088
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2022-06-22 00:23:32 +0430

    fix set job command

--- main.py
+++ main.py
@@ -265,16 +265,17 @@ async def set_job(ctx: SlashContext, employee, rank, taxi_code, ic_name, license
             roles = get_ranks_roles_by_id(ctx.guild)
             await member.add_roles(roles[884815982110060635])
             await member.add_roles(roles[884815998249758830])
-            await member.edit(nick=f"[{roster}] {ic_name}")
+            await member.edit(nick=f"[{taxi_code}] {ic_name}")
             embedVar = discord.Embed(title=f"Set Job Report", description=f"Successfully Signed {member.mention}")
             await ctx.send(embed=embedVar)
             staff_channel = ctx.guild.get_channel(staff_update_channel_id)
             await staff_channel.send(f"**Additional Staff update [{jdatetime.datetime.now().strftime(dt_format)}]** 📌\n" 
-                                    f"{member.mention} Has Been joined To SSTX and Will Be Known as [{roster}], Welcome!\n"
+                                    f"{member.mention} Has Been joined To SSTX and Will Be Known as [{taxi_code}], "
+                                     f"Welcome!\n "
                                      f"Author: {ctx.author.mention}")
             if license.lower() == "no":
                 await member.add_roles(roles[920018697924522074])
-            tx = TxEmployee(ic_name, roster, member.id, "")
+            tx = TxEmployee(ic_name, taxi_code, member.id, "")
             add_mcs_to_db([tx])
             
 
